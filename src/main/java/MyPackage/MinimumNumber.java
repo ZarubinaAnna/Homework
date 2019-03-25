06855f5108b33d541dbd060f54e864d0669e16bb
ZarubinaAnna <starmist2013@gmail.com>
2019-03-25 19:34:34 +0300

    Minimum int/long/double number out of two is selected.

---
+++ src/main/java/MyPackage/MinimumNumber.java
@@ -0,0 +1,14 @@
+package MyPackage;
+
+public class MinimumNumber {
+    public static void main(String[] args) {
+
+        System.out.println(Math.min(1, 10));
+        System.out.println(Math.min(1234567L, 987654L));
+        System.out.println(Math.min(1.5, 2.5));
+
+    }
+    }
+
+
+
